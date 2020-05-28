696fa7f3d6e975a9c14737529acd2271d36425d6
Made Umbu Paskalis Putra Arimbawa <made.umbu@ti.ukdw.ac.id>
2020-05-29 06:54:38 +0700

    update Delete

--- app/src/main/java/com/example/bookmarkinaja/Data.java
+++ app/src/main/java/com/example/bookmarkinaja/Data.java
@@ -1,13 +1,19 @@
 package com.example.bookmarkinaja;
 
+import android.app.AlertDialog;
 import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +49,17 @@ public class Data extends AppCompatActivity {
         listViewBook = findViewById(R.id.ListBook);
         bookList = new ArrayList<>();
 
+        listViewBook.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Book book = bookList.get(position);
+
+                showUpdateDelet(book.getBookId(),book.getBookJenis());
+
+                return false;
+            }
+        });
 
     }
     @Override
@@ -68,4 +85,72 @@ public class Data extends AppCompatActivity {
         }
     });
     }
+
+    private void showUpdateDelet(final String bookId, String bookJenis){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.update_delete, null);
+        builder.setView(dialogView);
+
+        final TextView jenis = dialogView.findViewById(R.id.textJenisaja);
+        final EditText jenisbaru = dialogView.findViewById(R.id.editJenis);
+        final EditText judulbaru = dialogView.findViewById(R.id.editJudul);
+        final Spinner spinnerbaru = dialogView.findViewById(R.id.spinnernew);
+        final Button update = dialogView.findViewById(R.id.btnUpdate);
+        final Button delete = dialogView.findViewById(R.id.btnDelete);
+
+
+        builder.setTitle("Update data" + bookJenis);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jenis = jenisbaru.getText().toString().trim();
+                String judul = judulbaru.getText().toString().trim();
+                String spinner = spinnerbaru.getSelectedItem().toString();
+
+                updateData(bookId, jenis,judul,spinner);
+                alertDialog.dismiss();
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteData(bookId);
+            }
+        });
+
+
+
+
+
+    }
+    private boolean updateData(String id, String jenis, String judul, String spinner){
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("book").child(id);
+
+        Book book = new Book(id,jenis,judul,spinner);
+
+        databaseReference.setValue(book);
+
+        Toast.makeText(this, "Data Berhasil di ganti", Toast.LENGTH_LONG).show();
+
+
+        return true;
+
+    }
+
+    private void deleteData(String bookId){
+        DatabaseReference drbook = FirebaseDatabase.getInstance().getReference("book").child(bookId);
+
+        drbook.removeValue();
+
+        Toast.makeText(this, "Data Berhasil di hapus", Toast.LENGTH_LONG).show();
+    }
+
 }
